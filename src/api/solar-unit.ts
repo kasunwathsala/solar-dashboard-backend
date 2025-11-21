cd381cbbc4e493d8e81c532be37371546b44c0c6
kasunwathsala <wathsala.nilaweera2001@gmail.com>
2025-11-21 23:01:55 +0530

    Add authentication and authorization middleware to energy generation and solar unit routes

--- src/api/solar-unit.ts
+++ src/api/solar-unit.ts
@@ -1,31 +1,26 @@
 import express from "express";
-
-import { 
-  getAllSolarUnits, 
-  createSolarUnit, 
-  getSolarUnitById, 
-  updateSolarUnit, 
-  deleteSolarUnit, 
-  getSolarUnitsByClerkUserId,
-  getSolarUnitForUser
+import {
+getAllSolarUnits,
+createSolarUnit,
+getSolarUnitById,
+updateSolarUnit,
+deleteSolarUnit,
+getSolarUnitForUser,
 } from "../application/solar-unit";
 import { authenticationMiddleware } from "./middlewares/authentication-middleware";
 import { authorizationMiddleware } from "./middlewares/authorization-middleware";
 
 const solarUnitRouter = express.Router();
 
-// Public routes
-solarUnitRouter.route("/").get(getAllSolarUnits).post(createSolarUnit);
-
-// User-specific routes  
-solarUnitRouter.route("/users/:clerkUserId").get(getSolarUnitsByClerkUserId);
-solarUnitRouter.route("/me").get(authenticationMiddleware, getSolarUnitForUser);
-
-// Individual solar unit routes
+// solarUnitRouter.route("/").get(getAllSolarUnits).post(createSolarUnitValidator, createSolarUnit);
+solarUnitRouter.route("/").get(authenticationMiddleware, authorizationMiddleware, getAllSolarUnits).post(authenticationMiddleware, authorizationMiddleware, createSolarUnit);
 solarUnitRouter
-  .route("/:id")
-  .get(getSolarUnitById)
-  .put(updateSolarUnit)
-  .delete(deleteSolarUnit);               
+.route("/:id")
+  // .get(getSolarUnitById)
+  // .put(updateSolarUnit)
+  // .delete(deleteSolarUnit);
+  .get(authenticationMiddleware, authorizationMiddleware, getSolarUnitById)
+  .put(authenticationMiddleware, authorizationMiddleware, updateSolarUnit)
+  .delete(authenticationMiddleware, authorizationMiddleware, deleteSolarUnit);
 
 export default solarUnitRouter;
